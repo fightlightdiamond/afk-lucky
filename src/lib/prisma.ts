622d9fc9483461fd68177f09353e9f8368e9e32d
Phạm Minh Cương <167342775+fightlightdiamond@users.noreply.github.com>
2025-08-20 05:04:30 +0700

    feat: configure prisma logs

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const logLevels: Prisma.LogLevel[] =
+  process.env.NODE_ENV === "development"
+    ? ["query", "info", "warn", "error"]
+    : ["warn", "error"];
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["query", "info", "warn", "error"],
+    log: logLevels,
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
